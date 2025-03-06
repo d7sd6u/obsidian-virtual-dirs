0b5065d425d7ad6021c59b100dd1c78e7414d4c5
d7sd6u <58745001+d7sd6u@users.noreply.github.com>
2025-03-06 19:50:16 +0400

    1.0.0

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -8,7 +8,7 @@ if you want to view the source, please visit the github repository of this plugi
 */
 `;
 
-const prod = process.argv[3] === "production";
+const prod = process.argv[2] === "production";
 
 const context = await esbuild.context({
 	banner: {
